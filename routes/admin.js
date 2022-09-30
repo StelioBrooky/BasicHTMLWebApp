1496b7d15f7fd77f8a46ad0c759a552dfd2bcc74
Aaris.Baskaran <aaris.baskaran@gmail.com>
2022-09-30 16:22:11 +1300

    added initial ejs stuff

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,10 @@ const path = require('path');
 router.get('/', (req, res) => {
     //res.send('<h1>Hello There</h1>');
 
-    res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
     // instead of res.send, we can use res.sendFile to send a file
 
-    // res.render('index'); - for ejs
+    res.render('index', {'pageTitle': 'This is the Admin Page'});
 });
 
 module.exports = router;
