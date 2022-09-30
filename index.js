18a4c7090c4513141a656360e09b05bb778e2779
Aaris.Baskaran <aaris.baskaran@gmail.com>
2022-09-30 16:57:01 +1300

    changed routing system to only one routes file, added some ejs stuff

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ express = require('express');
 
 const app = express();
 const routes = require('./routes/routes.js');
-const adminroutes = require('./routes/admin.js');
+//const adminroutes = require('./routes/admin.js');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use('/admin', adminroutes);
+//app.use('/admin', adminroutes);
 app.use('/', routes);
 
 const server = http.createServer(app);
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     //res.send('<h1>Hello World!!!</h1>');
-    res.render('index', {'pageTitle': 'This is the Home Page'});
+    res.render('index', {pageTitle: 'Home Page', name:''});
+});
+
+router.get('/admin', (req, res) => {
+    res.render('index', {pageTitle: 'Admin Page', name:'Admin'});
 });
 
 module.exports = router;
