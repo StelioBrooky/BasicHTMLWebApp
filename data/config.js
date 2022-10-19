5fde3aef8cdefabcbe5bb9dc583f479998aede3b
StelioBrooky <brookystel@myvuw.ac.nz>
2022-10-19 13:26:23 +1300

    database improvements maybe add is added

--- data/config.js
+++ data/config.js
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set, child, update, remove } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+function StartFirebase(){
 const firebaseConfig = {
   apiKey: "AIzaSyCpHQllzvmwS2biXRBUawK4jIdPsB0OdBw",
   authDomain: "nwen304-135s.firebaseapp.com",
@@ -18,4 +20,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+
+  return getDatabase(app);
+
+}
+
+export default StartFirebase;
