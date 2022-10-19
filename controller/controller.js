f356285ecd51a5c91a607f9455105a9901d93a11
StelioBrooky <brookystel@myvuw.ac.nz>
2022-10-19 16:56:25 +1300

    added functioning delete and updtaed data items

--- controller/controller.js
+++ controller/controller.js
@@ -4,7 +4,7 @@ const StartFirebase = require('../data/config');
 const reptile = require('../model/model');
 const db = StartFirebase();
 
-const {set, ref} = require("firebase/database");
+const {set, ref, get, child, remove} = require("firebase/database");
 
 exports.getIndex = (req, res) => {
     res.render('index', {pageTitle: 'Home Page', name:'', reptiles: reptile.fetchAll()});
@@ -17,11 +17,23 @@ exports.getAddPage = (req, res) => {
 exports.postAddPage = (req, res) => {
     var reptileName = req.body.name;
     var reptileId = req.body.id;
+    var reptileDiet = req.body.diet;
+    var reptileLocation = req.body.location;
+    var reptileLifeExpectancy = req.body.lifeExpectancy;
+    var reptileScientificName = req.body.scientificName;
+    var reptileEnclousure = req.body.enclosure;
+    var reptileDescription = req.body.description;
 
     if(req.body.action == 'add'){
 
         set(ref(db, 'reptiles/' + reptileId), {
-            name: reptileName
+            name: reptileName,
+            diet: reptileDiet,
+            location: reptileLocation,
+            lifeExpectancy: reptileLifeExpectancy,
+            scientificName: reptileScientificName,
+            enclosure: reptileEnclousure,
+            description: reptileDescription
         })
         .then(() => {
             console.log('Data saved!');
@@ -36,7 +48,41 @@ exports.postAddPage = (req, res) => {
         console.log(reptile.fetchAll());
         res.render('index', {pageTitle: 'Home Page', name:'', reptiles: reptile.fetchAll()});
     }
+
     else if(req.body.action == 'delete'){
+        remove(child(ref(db), 'reptiles/' + reptileId))
+        .then(() => {
+            console.log('Data removed!');
+            res.render('index', {pageTitle: 'Home Page', name:'', reptiles: reptile.fetchAll()});
+        })
+        .catch((error) => {
+            console.log('Data not removed: ' + error);
+        });
         console.log('delete');
+    }
+
+    else if(req.body.action == 'update'){
+        console.log('update');
+    }
+    else if(req.body.action == 'select'){
+        const dbref = ref(db);
+        get(child(dbref, 'reptiles/' + reptileId)).then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log(snapshot.val());
+                req.body.name = snapshot.val().name;
+                req.body.diet = snapshot.val().diet;
+                req.body.location = snapshot.val().location;
+                req.body.lifeExpectancy = snapshot.val().lifeExpectancy;
+                req.body.scientificName = snapshot.val().scientificName;
+                req.body.enclosure = snapshot.val().enclosure;
+                req.body.description = snapshot.val().description;
+
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+        console.log('select');
     }
 }
