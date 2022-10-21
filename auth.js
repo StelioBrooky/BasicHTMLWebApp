4f23761ef9317b7272e64bbf48ca152729bf773d
Arnan Jeram <arnanjeram@Arnans-MacBook-Pro.local>
2022-10-21 19:33:40 +1300

    fixing google login heroku

--- auth.js
+++ auth.js
@@ -4,10 +4,18 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const GOOGLE_CLIENT_ID = '999236982179-rvfglcmbhdtlpbsv4bk6rc7gu789hvlh.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
 
+CB1 = "http://localhost:3000/auth/google/callback";
+CB2 = 'https://reptile-resource.herokuapp.com/google/callback';
+
+const port = process.env.PORT || 3000;
+
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback" || "https://reptile-resource.herokuapp.com/google/callback",
+    callbackURL: CB2,
+    if (port = 3000){
+        callbackURL = CB1;
+    },
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
