abcca52adc1afc6e72537ddb847db0b207fa1424
Aaris.Baskaran <aaris.baskaran@gmail.com>
2022-10-22 19:45:06 +1300

    actually fixed google auth

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const GOOGLE_CLIENT_ID = '999236982179-rvfglcmbhdtlpbsv4bk6rc7gu789hvlh.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
 
-CB1 = "http://localhost:3000/auth/google/callback";
+CB1 = "http://localhost:3000/google/callback";
 CB2 = 'https://reptile-resource.herokuapp.com/google/callback';
 
 const port = require('./index.js')
@@ -12,10 +12,7 @@ const port = require('./index.js')
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: CB2,
-    if (port = 3000) {
-        callbackURL = CB1;
-    },
+    callbackURL: (port == 3000) ? CB1 : CB2,
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
